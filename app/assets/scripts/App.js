e206baeec4266f919ad136468de63d1e06567005
Martha <marthaduque77@gmail.com>
2023-07-19 16:07:09 -0400

    Completed sticky header functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import '../styles/styles.css'
 import MobileMenu from "./modules/MobileMenu"
 import ReavealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
+let stickyHeader = new StickyHeader()
 new ReavealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new ReavealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
